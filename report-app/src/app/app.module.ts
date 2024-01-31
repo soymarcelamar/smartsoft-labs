9163d588e6a6f081655fa47a5b2b393fe7650056
Marcela Martínez <63815530+soymarcelamar@users.noreply.github.com>
2024-01-31 13:35:33 -0500

    2# Install Angular Material / Header & Footer

--- report-app/src/app/app.module.ts
+++ report-app/src/app/app.module.ts
@@ -3,14 +3,22 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './basic/header/header.component';
+import { FooterComponent } from './basic/footer/footer.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatToolbarModule,
   ],
   providers: [
     provideClientHydration()
