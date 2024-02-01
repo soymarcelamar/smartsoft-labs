9f9f733f87f227ea7a988293994cebd9deeac56e
Marcela Martínez <63815530+soymarcelamar@users.noreply.github.com>
2024-02-01 11:42:49 -0500

    #6 Get Data from API

--- report-app/src/app/components/dashboard/dashboard.component.ts
+++ report-app/src/app/components/dashboard/dashboard.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DashboardService } from '../../services/dashboard/dashboard.service';
 
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit{
 
+  data: any[] = [];
+
+  constructor(private dashboardService: DashboardService) { }
+
+  ngOnInit(): void {
+    this.completeData();
+  }
+
+  completeData() {
+    this.dashboardService.getData().subscribe(data => {
+      this.data = data;
+      console.log(this.data)
+    });
+  };
 }
